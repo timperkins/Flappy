4991443a38069237b8082e66bd5dec3b161bd7f4
Tim Perkins <tjp503@gmail.com>
2018-09-20 23:30:11 -0700

    Initial commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	public float upForce = 200f;
+
+	private bool isDead = false;
+	private Rigidbody2D rb;
+	private Animator anim;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		if (isDead == false) {
+			if (Input.GetMouseButtonDown(0)) {
+				rb.velocity = Vector2.zero;
+				rb.AddForce(new Vector2(0, upForce));
+				anim.SetTrigger("Flap");
+			}
+		}
+	}
+
+	void OnCollisionEnter2D() {
+		rb.velocity = Vector2.zero;
+		isDead = true;
+		anim.SetTrigger("Die");
+		GameControl.instance.BirdDied();
+	}
+}
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.velocity = new Vector2(GameControl.instance.scrollSpeed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameControl.instance.isGameOver) {
+			rb.velocity = Vector2.zero;
+		}
+	}
+}
